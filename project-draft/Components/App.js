3432947d59154d4441f063c072fae8f40165bd4a
Zach Kornas <zkornas@uw.edu>
2022-05-23 14:42:51 -0700

    ate no crumbs

--- project-draft/Components/App.js
+++ project-draft/Components/App.js
@@ -1,9 +1,35 @@
 import React, { useState } from 'react'; //import React Component
+import React from 'react';
 
 function App(props) {
+
+  //Your work goes here
+
+  const [displayedData, displayedDataSetter] = useState(props.gameData);
+
+  function applyFilter(nameToFilter, booleanRunners) {
+    let gameDataHolder = props.gameData;
+    if (nameToFilter == "") {
+      displayedDataSetter(gameDataHolder);
+    } else {
+      let arr = gameDataHolder.filter(game => game.winner == nameToFilter);
+      displayedDataSetter(arr);
+      if(booleanRunners == true) {
+        let arr2 = props.gameData.filter(game => game.runner_up == nameToFilter || game.winner == nameToFilter);
+        displayedDataSetter(arr2);
+      }
+    }
+  }
+  
+  //get sorted list of unique teamNames. reduce array of objects into array of strings, 
+  //convert to Set to get uniques, spread back into array, and sort 
+  const uniqueTeamNames = [...new Set(props.gameData.reduce((all, current) => {
+    return all.concat([current.winner, current.runner_up]);
+  }, []))].sort();
+
   return (
     <nav class="navbar navbar-expand-lg navbar-light">
-  <img class="nav-logo" src="./img/logo.png" width="50px" height="50px"/>
+  <img class="nav-logo" src="./img/logo.png" width="50px" height="50px">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -16,25 +42,25 @@ function App(props) {
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <div class="drop-item">
-            <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+            <input type="checkbox" class="form-check-input" id="dropdownCheck">
             <label class="form-check-label" for="dropdownCheck">
               MGH
             </label>
           </div>
           <div class="drop-item">
-            <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+            <input type="checkbox" class="form-check-input" id="dropdownCheck">
             <label class="form-check-label" for="dropdownCheck">
               GWN
             </label>
           </div>
           <div class="drop-item">
-            <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+            <input type="checkbox" class="form-check-input" id="dropdownCheck">
             <label class="form-check-label" for="dropdownCheck">
               KNE
             </label>
           </div>
           <div class="drop-item">
-            <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+            <input type="checkbox" class="form-check-input" id="dropdownCheck">
             <label class="form-check-label" for="dropdownCheck">
               ODE
             </label>
@@ -49,25 +75,25 @@ function App(props) {
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 Basement
               </label>
             </div>
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 First Floor
               </label>
             </div>
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 Second Floor
               </label>
             </div>
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 Third or Higher
               </label>
@@ -82,19 +108,19 @@ function App(props) {
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 Private Room
               </label>
             </div>
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 Individual Stall
               </label>
             </div>
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 Urinal
               </label>
@@ -109,31 +135,31 @@ function App(props) {
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 1 Star
               </label>
             </div>
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 2 Star
               </label>
             </div>
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 3 Star
               </label>
             </div>
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 4 Star
               </label>
             </div>
             <div class="drop-item">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
               <label class="form-check-label" for="dropdownCheck">
                 5 Star
               </label>
@@ -145,7 +171,7 @@ function App(props) {
 
     </ul>
     <form class="form-inline my-2 my-lg-0 search-button">
-      <input class="" type="search" placeholder="Search" aria-label="Search"/>
+      <input class="" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
@@ -154,4 +180,3 @@ function App(props) {
 }
 
 export default App;
-
