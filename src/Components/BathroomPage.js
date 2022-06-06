450023f7eb7d6084d462e3dadb03e1b2f8cb9977
Nic Chin <platinum1584@hotmail.com>
2022-06-05 21:42:44 -0700

    fixed bathroom params

--- src/Components/BathroomPage.js
+++ src/Components/BathroomPage.js
@@ -5,23 +5,26 @@ import _ from 'lodash';
 
 export default function BathroomPage(props) {
 
-    const bathroomID = useParams().bathroomID
-    console.log(bathroomID)
-    // temporarily hard-coded to bathroom #13
-    let bathroom = _.find(props.data, { id: 13 });
-    console.log(props.data)
+    const bathroomID = useParams().bathroomID;
+    console.log(bathroomID);
+    console.log(typeof bathroomID);
+    let bathroom = props.data[parseInt(bathroomID) - 1];
+    console.log(bathroom);
+
+    console.log(props.data);
 
     if (!bathroom) return <h2>No bathroom specified</h2>
 
     return (
         <div>
             <h2 className="bathroom-header">{bathroom.building} - {bathroom.floor}</h2>
-            <img src={"/img/" + bathroom.src} alt={"Bathroom in " + bathroom.building + " on the " + bathroom.floor} />
-            <p>If possible, we should have the bathroom's location on the map appear here</p>
+            <img src={"/" + bathroom.src} alt={"Bathroom in " + bathroom.building + " on the " + bathroom.floor} className="bathroomPic" />
+            {/* <p>If possible, we should have the bathroom's location on the map appear here</p> */}
+            <hr className="line-seperator" />
             <h2 className="bathroom-header">Reviews</h2>
-            <form>
+            <form className="review" >
                 <div className="form-group">
-                    <label for="review-input">Leave a review!
+                    <label htmlFor="review-input">Leave a review!
                         <textarea id="review-input" name="review-input" className="form-control" />
                         <button type="submit" className="btn btn-warning">Submit</button>
                     </label>
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,12 +1,8 @@
 import React from 'react'; //import React Component
-import { Link } from 'react-router-dom';
 
 export default function Map(props) {
     return (
         <div>
-            <Link className="map-link" to="/search">
-                <h2 className="map-title">Find a Bathroom!</h2>
-            </Link>
             <div className="map">
                 <iframe title="DUB DUMPS bathroom map" className="map-image" src="https://snazzymaps.com/embed/398528" width="80%" height="550px"></iframe>
                 <p>Use our map to find nearby bathrooms!</p>
