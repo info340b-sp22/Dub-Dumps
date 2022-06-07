656cabd19bf9469061bdd5bb5eb688480a06e993
ameliachen3 <91903546+ameliachen3@users.noreply.github.com>
2022-06-06 22:55:17 -0700

    updated css and hamburger

--- src/Components/StructuredSearch.js
+++ src/Components/StructuredSearch.js
@@ -1,6 +1,8 @@
 import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 import React, { useState } from 'react'; //import React Component
 import { Link, Outlet } from 'react-router-dom';
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 export function StructuredSearch(props) {
     const [bldgSelected, setBldg] = useState('');
@@ -57,36 +59,28 @@ export function StructuredSearch(props) {
 
 
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light mb-4">
-                {/* should we leave the logo out? also we still need to implement the hamburger menu functionality for mobile screens*/}
-                {/* <img className="nav-logo" src="./img/logo.png" width="50px" height="50px" /> */}
-                {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button> */}
-                <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item dropdown"></li>
-                        <li className="nav-item dropdown d-flex">
-                            <select id="buildingSelect" className="nav-category dropdown-toggle form-select select-dropdown" onChange={changeBldg} >
-                                <option value="">Building</option>
-                                {buildings}
-                            </select>
-                            <select id="floorSelect" className="nav-category dropdown-toggle form-select" onChange={changeFloor} >
-                                <option value="">Floor</option>
-                                {floors}
-                            </select>
-                            <select id="locationSelect" className="nav-category dropdown-toggle form-select" onChange={changeLocation} >
-                                <option value="">Location</option>
-                                {locations}
-                            </select>
-                            <div className="col-auto">
-                                <Link to="/search" id="submitButton" type="submit" className="btn btn-warning" onClick={handleClick}>Search!</Link>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav >
+        <div><Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" className="hamburger" />
+            <Navbar.Collapse id="responsive-navbar-nav navBar">
+                <Nav className="mr-auto">
+                    <select id="buildingSelect" className="navDropDown" onChange={changeBldg} >
+                        <option value="">Building</option>
+                        {buildings}
+                    </select>
+                    <select id="floorSelect" className="navDropDown" onChange={changeFloor} >
+                        <option value="">Floor</option>
+                        {floors}
+                    </select>
+                    <select id="locationSelect" className="navDropDown" onChange={changeLocation} >
+                        <option value="">Location</option>
+                        {locations}
+                    </select>
+                    <div className="col-auto">
+                        <Link to="/search" id="submitButton" type="submit" className="btn btn-warning" onClick={handleClick}>Search!</Link>
+                    </div>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
             <Outlet />
         </div >
     )
